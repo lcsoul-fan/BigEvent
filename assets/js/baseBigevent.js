f561003ed6157291c46d66e30e6d70210d7750c8
lcosul <383289357@qq.com>
2021-10-03 16:23:58 +0800

    首页模块开发完成，新增index.js文件，index.css文件，baseBigevent.js文件进行了通用ajax请求方法

--- assets/js/baseBigevent.js
+++ assets/js/baseBigevent.js
@@ -1,3 +1,18 @@
 $.ajaxPrefilter(function(options) {
+    //统一拼接请求地址
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    //统一对需要权限的定制增加 headers
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            'Authorization': sessionStorage.getItem('token') || ""
+        }
+    }
+    //统一对未授权登录进行处理，返回login页面
+    options.complete = function(res) {
+        console.log(res);
+        if (res.responseJSON.status == 1 && res.responseJSON.message === '身份认证失败！') {
+            sessionStorage.removeItem('token');
+            location.href = 'login.html';
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+// const layuiAll = require("../lib/layui/layui.all")
+
+$(function() {
+    getUserInfo()
+    var layer = layui.layer
+    $('#logout').on('click', function() {
+        layer.confirm('确认退出吗？', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            sessionStorage.removeItem('token');
+            location.href = "login.html"
+            layer.close(index);
+        });
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        method: 'GET',
+        // headers: { 'Authorization': sessionStorage.getItem('token') || "" },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderUserInfo(res.data)
+        }
+
+    })
+}
+
+function renderUserInfo(userinfo) {
+    var username = userinfo.nickname || userinfo.username;
+    $('.welcome').html('欢迎&nbsp;&nbsp;' + username)
+    if (userinfo.user_pic !== null) {
+        $('.layui-nav-img').attr('src', userinfo.user_pic);
+        $('.avatar-default').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        $('.avatar-default').html(username[0].toUpperCase()).show();
+    }
+}
