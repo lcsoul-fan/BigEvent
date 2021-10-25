8d7b5ec961a4be8a7f6af6a965a2750a4ee144b3
lcsoul <383289357@qq.com>
2021-10-25 14:48:45 +0800

    把127.0.0.1换成实际Ip

--- assets/js/baseBigevent.js
+++ assets/js/baseBigevent.js
@@ -1,7 +1,6 @@
 $.ajaxPrefilter(function(options) {
         //统一拼接请求地址
-        console.log(options.url);
-        options.url = 'http://127.0.0.1' + options.url;
+        options.url = 'http://10.252.187.247' + options.url;
         //统一对需要权限的定制增加 headers
         if (options.url.indexOf('/my/') !== -1) {
             options.headers = {
@@ -10,6 +9,7 @@ $.ajaxPrefilter(function(options) {
         }
         //统一对未授权登录进行处理，返回login页面
         options.complete = function(res) {
+            console.log(res);
             if (res.responseJSON.status == 1 && res.responseJSON.message === '身份认证失败！') {
                 sessionStorage.removeItem('token');
                 location.href = 'login.html';
@@ -17,6 +17,7 @@ $.ajaxPrefilter(function(options) {
                 location.href = 'login.html';
 
             }
+
         }
 
     })
